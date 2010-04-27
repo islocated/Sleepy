34fbc9e6e17892bc68c08d4e2ef9a2d57fd6dffc
Minh Nguyen <islocated@gmail.com>
2010-04-27 18:54:56 -0500

    Added magnifying slider to adjust values within the magnify view.

--- src/com/isnotok/sleep/view/MagnifyView.java
+++ src/com/isnotok/sleep/view/MagnifyView.java
@@ -5,15 +5,25 @@ import java.io.IOException;
 import java.util.zip.DataFormatException;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Slider;
 import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.nebula.widgets.gallery.DefaultGalleryGroupRenderer;
 import org.eclipse.nebula.widgets.gallery.DefaultGalleryItemRenderer;
@@ -24,10 +34,12 @@ import com.isnotok.sleep.model.PakFile;
 import com.isnotok.sleep.model.PakRecord;
 import com.isnotok.sleep.model.ResourceManager;
 
+//Implemtn ISelectionProvider if we want this view to return the zoom
 public class MagnifyView extends ViewPart implements ISelectionListener{
 	public final static String ID = "com.isnotok.sleep.view.MagnifyView";
 	private PakFile pakfile;
 	private Gallery gallery;
+	private Slider slider;
 
 	public MagnifyView() {
 		// TODO Auto-generated constructor stub
@@ -37,10 +49,20 @@ public class MagnifyView extends ViewPart implements ISelectionListener{
 	public void createPartControl(final Composite parent) {
 		// TODO Auto-generated method stub
 		
+		Composite canvas = new Composite(parent, SWT.NONE);
+		GridData gridData;
+		
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		canvas.setLayout(gridLayout);
+		
 		//Sets up the ability for this view to get selection events from all views
 		getSite().getPage().addSelectionListener(this);
 
-		gallery = new Gallery(parent, SWT.V_SCROLL | SWT.VIRTUAL);
+		gallery = new Gallery(canvas, SWT.V_SCROLL | SWT.VIRTUAL);
+		gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gridData.horizontalSpan = 2;
+		gallery.setLayoutData(gridData);
 		
 		final DefaultGalleryGroupRenderer gr = new DefaultGalleryGroupRenderer();
 		gr.setMinMargin(2);
@@ -115,6 +137,42 @@ public class MagnifyView extends ViewPart implements ISelectionListener{
 				}
 			}
 		});
+		
+		/*
+		Image img = PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_ADD);
+		Label label = new Label(canvas, SWT.BORDER);
+		label.setImage(img);
+		gridData = new GridData(SWT.LEFT, SWT.CENTER, true, false);
+		//gridData.widthHint = 8;
+		label.setLayoutData(gridData);
+		*/
+		
+		slider = new Slider(canvas, SWT.HORIZONTAL);
+		slider.setValues(2, 2, 5, 1, 1, 1);
+		
+		//slider.setMinimum(32);
+		//slider.setMaximum(256);
+		//slider.setIncrement(32);
+		
+		gridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		slider.setLayoutData(gridData);
+		slider.addSelectionListener(new SelectionListener() {
+			
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				int zoom = (int) (16 * Math.pow(2, slider.getSelection()));
+				System.out.println("zoom: " + zoom);
+				gr.setItemSize(zoom, zoom);
+			}
+			
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		//getSite().setSelectionProvider(this);
+	    //getSite().setSelectionProvider(slider);
 	}
 
 	@Override
@@ -139,5 +197,4 @@ public class MagnifyView extends ViewPart implements ISelectionListener{
 			}
 		}
 	}
-
 }
